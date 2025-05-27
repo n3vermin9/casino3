b7c2a705b5e1e6d9e0b78c0b460140f801390b0b
n3vermin9 <89288662119s@mail.ru>
2025-05-27 10:58:23 +0300

    fix bug

--- gamesList.js
+++ gamesList.js
@@ -1,5 +1,5 @@
 export const gamesList = [
-  {image: 'slots.png', name: 'Slots', link: 'slots.html', filter: true},
+  {image: 'imgs/slots.png', name: 'Slots', link: 'slots.html', filter: true},
   {image: 'ghost.png', name: 'Ghost', link: 'ghost.html', filter: false},
   {image: 'goldChest.png', name: 'Pirates', link: 'pirates.html', filter: true},
   {image: 'wheel.png', name: 'Fortune Wheel', link: 'fortuneWheel.html', filter: true},
