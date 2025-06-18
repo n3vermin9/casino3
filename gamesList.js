493cd8c1326dcf10a919ccea8213f0d89525a172
n3vermin9 <89288662119s@mail.ru>
2025-06-18 22:06:56 +0300

    minor changes

--- gamesList.js
+++ gamesList.js
@@ -1,6 +1,6 @@
 export const gamesList = [
-  {image: 'imgs/slots.png', name: 'Slots', link: 'slots/slots.html', filter: true},
-  {image: 'imgs/ghost.png', name: 'Ghost', link: 'ghost/ghost.html', filter: false},
+  {image: 'imgs/slots.png', name: 'Tg Slots', link: 'slots/slots.html', filter: true},
+  {image: 'imgs/ghost.png', name: 'Ghost Crash', link: 'ghost/ghost.html', filter: false},
   {image: 'imgs/goldChest.png', name: 'Pirates', link: 'pirates/pirates.html', filter: true},
   {image: 'imgs/wheel.jpg', name: 'Fortune Wheel', link: 'fortuneWheel/fortuneWheel.html', filter: true},
 ]
